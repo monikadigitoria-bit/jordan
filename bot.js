810b0bc9cc1ab44d10b4010104e6727dd3a85d70
monikadigitoria-bit <monika.digitoria@gmail.com>
2026-05-18 13:05:52 +0530

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,50 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const TOKEN = "8726969586:AAFI2DKl3q_QSlEOZ8ooYQeepPTMcOo4LP8";
+const CHANNEL_LINK = "https://t.me/+Q0TN7aLjeIw1OWM1";
+const VIP_LINK = "https://t.me/+tu4ddDJD36w2MTI1";
+
+const bot = new TelegramBot(TOKEN, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+
+  const text = `Welcome to JORDAN AVIATOR 
+
+This platform is created for educational purposes only.
+
+Get daily market insights, learning-based chart analysis & easy-to-understand trading concepts.
+Improve your knowledge step by step with us.
+
+🚀 Learn • Understand • Grow
+📚 Educational Content Only – No Financial Advice
+
+Tap the buttons below 👇`
+  const options = {
+    reply_markup: {
+      inline_keyboard: [
+        [{ text: "📢 Join Official Channel", url: CHANNEL_LINK }],
+        [{ text: "I Joined 👍", callback_data: "joined" }]
+      ]
+    }
+  };
+
+  bot.sendMessage(chatId, text, options);
+
+  setTimeout(() => {
+    bot.sendMessage(chatId,
+      "⚠️ Disclaimer:\nWe do NOT provide investment advice.\nAll market analysis is ONLY for educational purposes."
+    );
+  }, 1000);
+});
+
+bot.on('callback_query', (query) => {
+  if (query.data === 'joined') {
+    bot.sendMessage(query.message.chat.id,
+     "✅ Thank you for joining!\n\nYou will now receive daily market insights and educational trading setups for learning purposes only.\n\n📚 This is not financial advice. Always do your own research before trading.\n\nStay tuned and keep learning! 📈"
+    );
+    bot.answerCallbackQuery(query.id);
+  }
+});
+
+console.log("Bot is running...");
